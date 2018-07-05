e8566d16a3c4fad280d0cf54c52c94c457940b8b
Jarek Wojciechowski <jarekwojo@gmail.com>
2018-07-05 15:59:22 -0400

    fix unused 'window' and 'document' warnings as well as groundwork for multiple cascading diplays

--- src/preindex.js
+++ src/preindex.js
@@ -1,27 +1,25 @@
+/* eslint-env browser */
 const Trianglify = require('trianglify');
 const { TimelineMax } = require('gsap');
 
 const tmaxOptions = {
   delay: 0,
-  repeat: -1,
-  repeatDelay: 0.25,
-  yoyo: true,
+  repeat: 0,
 };
 
 const svgShapes = [];
 const staggerVal = 0.00475;
 const duration = 1.5;
-const svgContainer = document.querySelector('.svg-container');
 
 const tmaxForward = new TimelineMax(tmaxOptions);
 
-const createAndInsertTrianglify = () => {
+const createAndInsertTrianglify = (container) => {
   const pattern = Trianglify({
     width: window.innerWidth,
     height: window.innerHeight,
   });
   const svg = pattern.svg();
-  svgContainer.appendChild(svg);
+  container.appendChild(svg);
   return svg.childElementCount;
 };
 
@@ -36,7 +34,7 @@ const animateTrianglify = (numTriangles) => {
       transformOrigin: 'center center',
       opacity: 0,
     },
-    ease: Elastic.easeInOut,
+    ease: 'Elastic.easeInOut',
     force3D: true,
   };
 
@@ -45,14 +43,27 @@ const animateTrianglify = (numTriangles) => {
       scale: 1,
       opacity: 1,
     },
-    ease: Elastic.easeInOut,
+    ease: 'Elastic.easeInOut',
     force3D: true,
   };
 
   tmaxForward.staggerFromTo(svgShapes, duration, staggerFrom, staggerTo, staggerVal, 0);
 };
 
+class ContainerManager {
+  constructor() {
+    this.containerList = [
+      document.querySelector('.svg-container'),
+    ];
+  }
+  getNextContainer() {
+    return this.containerList.pop();
+  }
+}
+
+const containerManager = new ContainerManager();
+
 window.onload = () => {
-  const numTriangles = createAndInsertTrianglify();
+  const numTriangles = createAndInsertTrianglify(containerManager.getNextContainer());
   animateTrianglify(numTriangles);
 };
